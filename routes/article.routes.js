89e073214f5c578e64f2afdc6b295a6ae2bcb0f8
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-12-04 10:42:52 +0330

    update

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -16,9 +16,4 @@ router.route('/limit/:num').get(getlimitArticle);
 router.route('/limit/:num/picked').get(getEditorPicked);
 router.route('/limit/:num/popular').get(getPopularArticles);
 router.route('/limit/:num/rest').get(getOtherArticles);
-router.route('/test/test').get(async (req, res) => {
-    const a = await model.find().count();
-    res.status(200).json(a);
-});
-
 module.exports = router;
