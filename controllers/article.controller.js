5ee87ed876816e79486715ddedf35c820287c840
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-12-06 02:40:42 +0330

    update

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -2,21 +2,34 @@ const model = require('../models/article.model');
 
 // Get all of the articles.
 exports.getAllArticles = async (req, res) => {
-    await model.find()
+
+    const lt = req.query.lt;
+
+    if(lt == 0){
+        await model.find().limit(12).sort({_id: -1})
         .then(doc => {
-            res.status(200).json({
-                status: 200,
-                doc: doc
-            })
+            res.status(200).json(doc);
         })
         .catch( err => {
             console.log(err);
         });
+    }
+    else {
+        await model.find({_id: { $lt: lt }}).limit(12).sort({_id: -1})
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch( err => {
+            console.log(err);
+        });
+    }
 }
 
 // Get one article.
 exports.getArticle = async (req, res) => {
+
     const slug = req.query.slug;
+
     await model.findOne({ slug: { $regex: slug }})
         .then(doc => {
             res.status(200).json(doc);
@@ -42,9 +55,10 @@ exports.createArticle = async (req, res) => {
 
 // Get limited articles. DESC sort
 exports.getlimitArticle = async (req, res) => {
+
     const limit = req.params.num;
 
-    await model.find().limit(limit).sort({"created": -1})
+    await model.find().limit(limit).sort({_id: -1})
         .then(doc => {
             res.status(200).json(doc);
         })
@@ -55,6 +69,7 @@ exports.getlimitArticle = async (req, res) => {
 
 // Get editor picked articles. DESC sort
 exports.getEditorPicked = async (req, res) => {
+
     const limit = req.params.num;
 
     await model.find({ editorpicked: true }).limit(limit)
@@ -68,6 +83,7 @@ exports.getEditorPicked = async (req, res) => {
 
 // Get popular articles
 exports.getPopularArticles = async (req, res) => {
+
     const limit = req.params.num;
 
     await model.find().limit(limit).sort({"visit": -1})
@@ -81,9 +97,10 @@ exports.getPopularArticles = async (req, res) => {
 
 // Get latest articles
 exports.getLatestArticles = async (req, res) => {
+    
     const limit = req.params.num;
 
-    await model.find().skip(5).limit(8).sort({'created': -1})
+    await model.find().skip(5).limit(8).sort({_id: -1})
         .then(doc => {
             res.status(200).json(doc);
         })
