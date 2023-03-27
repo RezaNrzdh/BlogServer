5f7073856451fb0050c511e6a61bc7545e5895b3
reza.nrzdh@gmail.com <reza.nrzdh@gmail.com>
2023-03-27 08:12:36 +0330

    update

--- controllers/comment/createComment.controller.js
+++ controllers/comment/createComment.controller.js
@@ -5,7 +5,9 @@ exports.createComment = async(req, res) => {
     let id = req.body.id;
     delete req.body.id;
 
-    await model.findByIdAndUpdate({ _id: id }, { $push: { comments: req.body } })
+    await model.updateOne(
+        { _id: id },
+        { $push: { comments: req.body } })
         .then( doc => {
             res.status(201).json({
                 status: 201,
